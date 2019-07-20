10d9d896643476c74e4c4e85738d7d8402f74f1b
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2019-07-20 01:04:12 +0100

    users contact data can now be submitted to database

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-// use \App\Contact;
+use \App\Contact;
 
 use Illuminate\Http\Request;
 
@@ -22,11 +22,21 @@ class ContactsController extends Controller
     public function storeContact()
     {
         //create a new post using the request data
-        //save it to d db
-        //and then redirect to the home page
+        //Contact is in model
+        $contact = new Contact; //this is instantiation in OOP
+        //call the Contact model properties
+        //set the fields to what d user typed in
+        $contact->fullname = request('full_name');
+        $contact->email = request('user_mail');
+        $contact->subject = request('subject');
+        $contact->body = request('message');
 
+        //save it to d db
+        $contact->save();
+        //and then redirect to the home page
+        return redirect('contact');
         // firstly (die and dump)
-        dd(request(['user_mail', 2]));
+        // dd(request(['user_mail']));
     }
 
 
