b75097b853bc3a72bd0cad346fe4118899776fee
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2019-07-22 18:44:20 +0100

    modified the users table by adding firstname column

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,4 +7,10 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     //
+    // firtly load a view
+    public function create()
+    {
+        return view ('frontend.user.signup.');
+    }
+
 }
